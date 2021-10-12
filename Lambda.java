24115aa7defe26d3f931cf6c9bf304ead9e1c43e
Hong Sung <hsung@uco.edu>
2021-10-11 19:54:04 -0500

    lambda function

---
+++ Lambda.java
@@ -0,0 +1,25 @@
+import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Lambda {
+
+    public static void main(String[] args) {
+
+        JButton button = new JButton("Enter");
+        ButtonListener listener = new ButtonListener();
+        button.addActionListener(listener);
+
+    }
+    
+}
+
+class ButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+       System.out.println("Enter button is pressed");
+    }
+
+}
